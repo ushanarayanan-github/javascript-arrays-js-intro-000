89aed83ec5dff2c1539ce42becf1934901348727
creative-buffer-9578 <cmuusha@gmail.com>
2020-07-03 04:44:59 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,8 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 function addElementToBeginningOfArray(myArray,element) {
    return [element, ...myArray]
 }
+
+function destructivelyAddElementToBeginningOfArray(myArray,element) {
+   myArray.unshift(element)
+   return myArray
+}
